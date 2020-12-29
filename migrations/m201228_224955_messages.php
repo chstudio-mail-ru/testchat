0434a277270d1c8be56282d7d812dd77dd63eb65
Denis Averkov <dmaverkov@gmail.com>
2020-12-29 18:38:58 +0300

    add messages table

---
+++ migrations/m201228_224955_messages.php
@@ -0,0 +1,55 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m201228_224944_users
+ */
+class m201228_224955_messages extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->execute("CREATE TABLE `messages` (
+              `id` int(11) NOT NULL,
+              `author_id` int(11) NOT NULL,
+              `date_add` int(11) NOT NULL,
+              `message` varchar(1024) NOT NULL,
+              `deleted` enum('0','1') NOT NULL DEFAULT '0'
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8;");
+
+        $this->execute("ALTER TABLE `messages`
+              ADD PRIMARY KEY (`id`),
+              ADD KEY `author_id` (`author_id`),
+              ADD KEY `date_add` (`date_add`);");
+
+        $this->execute("ALTER TABLE `messages`
+                MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+            COMMIT;");
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable("messages");
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m201228_224944_users cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
